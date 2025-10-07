65fb6758b05381661644fb5d1f745784b0e7959c
ISB Nhan <dtnhan@vn.isb.co.jp>
2025-10-07 14:48:49 +0700

    update config

--- route/index.js
+++ route/index.js
@@ -65,7 +65,7 @@ const route = (app) => {
    app.all('*', (req, res) => {
       res.status(404);
       if (req.accepts('html')) {
-         res.sendFile(path.join(__dirname, 'views', '404.html'));
+         res.sendFile(path.join(__dirname, '..', 'views', '404.html'));
       } else if (req.accepts('json')) {
          res.json({
             message: '404 Not Found',
